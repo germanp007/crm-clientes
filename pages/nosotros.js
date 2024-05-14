412b79b9001d10965fe01187b0287207dc0549e2
germanp007 <pintogerman281@gmail.com>
2024-05-14 15:42:38 -0300

    first commit

---
+++ pages/nosotros.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout";
+
+const nosotros = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Nosotros</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default nosotros;
