73dc2a3b84ccf065ad5c83557cb1217f31aa199f
germanp007 <pintogerman281@gmail.com>
2024-05-15 15:11:35 -0300

    first commit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="bg-gray-800 sm:w-1/3 lg:w-1/5">
+    <div className="bg-gray-800 sm:w-1/3 lg:w-1/5 p-3">
       <h2 className="font-bold text-white">Sidebar</h2>
+      <ul className="font-bold text-white">
+        <li>Contactos</li>
+        <li>Clientes</li>
+        <li>Nosotros</li>
+        <li>Ellos</li>
+      </ul>
     </div>
   );
 };
