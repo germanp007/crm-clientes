b1dd81039b8099b9dc0085b2ad8a920d76dddea5
germanp007 <pintogerman281@gmail.com>
2024-05-16 13:19:10 -0300

    creando validaciones y manejo de datos con formik y yup

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -1,9 +1,108 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 const NuevaCuenta = () => {
+  // Validacion del Formulario
+
+  const formik = useFormik({
+    initialValues: {
+      nombre: "",
+      apellido: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (valores) => {
+      console.log("enviando");
+      console.log(valores);
+    },
+  });
   return (
     <Layout>
-      <h1 className="text-2xl text-white font-light">Nueva Cuenta</h1>
+      <div className="flex flex-col w-screen">
+        <h1 className="text-2xl text-white font-light text-center">
+          Crear Nueva Cuenta
+        </h1>
+        <div className="flex justify-center mt-5">
+          <div className="w-full max-w-sm ">
+            <form
+              className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+              onSubmit={formik.handleSubmit}
+            >
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="nombre"
+                >
+                  Nombre
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="nombre"
+                  type="text"
+                  placeholder="Nombre Usuario"
+                  value={formik.values.nombre}
+                  onChange={formik.handleChange}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="apellido"
+                >
+                  Apellido
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="apellido"
+                  type="text"
+                  placeholder="Apellido Usuario"
+                  value={formik.values.apellido}
+                  onChange={formik.handleChange}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Email Usuario"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Password
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="password Usuario"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                ></input>
+              </div>
+              <input
+                type="submit"
+                className="bg-gray-800 w-full mt-5 p-2 text-white uppercase cursor-pointer hover:bg-slate-900"
+                value="Crear Cuenta"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
