19ef7a91e0fa73c8b9d4a376ebc5f1c45cc116ea
germanp007 <pintogerman281@gmail.com>
2024-07-24 11:29:36 -0300

    creando vistas pedidos clientes

--- components/Pedido.jsx
+++ components/Pedido.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Pedido = ({ pedido }) => {
-  return <h1>{pedido.total}</h1>;
+  const { id, total, cliente, estado } = pedido;
+  return (
+    <div className="mt-4 bg-white rounded p-6 md:grid md: grid-cols-2 md:gap-4 shadow-lg">
+      <div>
+        <p className="font-bold text-gray-800">Cliente: {id}</p>
+
+        <h2 className="font-bold text-gray-800">Estado Pedido: </h2>
+        <select className="mt-2 appearance-none bg-blue-600 border border-blue-600 text-white p-2 text-center rounded leading-tight focus:outline-none focus:bg-blue-600 focus:border-blue-500 uppercase text-xs font-bold">
+          <option value="completado">COMPLETADO</option>
+          <option value="pendiente">PENDIENTE</option>
+          <option value="cancelado">CANCELADO</option>
+        </select>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Pedido;
