147052a04d675b72e6302dc43e93578b2414d518
germanp007 <pintogerman281@gmail.com>
2024-08-02 12:11:17 -0300

    graficando mejores vendedores

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Frontend CRM de clientes NextJs-GraphQL
 
 user: german@german.com
 password: ger5564
+user: natita@gmail.com
+password: nati1978
+user: josem109@yahoo.net
+password: manu3333
--- pages/mejoresvendedores.js
+++ pages/mejoresvendedores.js
@@ -13,51 +13,6 @@ import {
 } from "recharts";
 import { gql, useQuery } from "@apollo/client";
 
-// const data = [
-//   {
-//     name: "Page A",
-//     uv: 4000,
-//     pv: 4400,
-//     amt: 2400,
-//   },
-//   {
-//     name: "Page B",
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210,
-//   },
-//   {
-//     name: "Page C",
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290,
-//   },
-//   {
-//     name: "Page D",
-//     uv: 2780,
-//     pv: 3908,
-//     amt: 2000,
-//   },
-//   {
-//     name: "Page E",
-//     uv: 1890,
-//     pv: 4800,
-//     amt: 2181,
-//   },
-//   {
-//     name: "Page F",
-//     uv: 2390,
-//     pv: 3800,
-//     amt: 2500,
-//   },
-//   {
-//     name: "Page G",
-//     uv: 3490,
-//     pv: 4300,
-//     amt: 2100,
-//   },
-// ];
-
 const MEJORES_VENDEDORES = gql`
   query MejoresVendedores {
     mejoresVendedores {
@@ -72,11 +27,37 @@ const MEJORES_VENDEDORES = gql`
 const MejoresVendedores = () => {
   const { data, loading, error } = useQuery(MEJORES_VENDEDORES);
 
-  console.log(data);
-  console.log(loading);
-  console.log(error);
-
   if (loading) return "Cargando...";
+  const { mejoresVendedores } = data;
+  const graphicVendedor = [];
+
+  mejoresVendedores.map((vendedor, index) => {
+    /* 
+    [
+      {
+        "__typename": "TopVendedores",
+        "vendedor": [
+          {
+            "__typename": "Usuario", <=== mejoresVendedores
+            "nombre": "German",
+            "apellido": "Pinto"
+          }
+                     ],
+        "total": 3640
+      }
+    ] 
+
+    */
+
+    // Del Objeto extraemos los datos q necesitamos
+    const { __typename, ...datosVendedor } = vendedor.vendedor[0];
+    //Agregamos los datos del Vendedor q extraemos al array graphicVendedor
+    graphicVendedor[index] = {
+      ...datosVendedor,
+      total: vendedor.total,
+    };
+  });
+  console.log(graphicVendedor);
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">Mejores Vendedores</h1>
@@ -84,7 +65,7 @@ const MejoresVendedores = () => {
         <BarChart
           width={600}
           height={400}
-          data={data}
+          data={graphicVendedor}
           margin={{
             top: 5,
             right: 30,
@@ -93,12 +74,12 @@ const MejoresVendedores = () => {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="nombre" />
           <YAxis />
           <Tooltip />
           <Legend />
           <Bar
-            dataKey="pv"
+            dataKey="total"
             fill="#3182CE"
             activeBar={<Rectangle fill="blue" stroke="blue" />}
           />
