1d1e00863764d8b693eefa886a77900d68cddb06
germanp007 <pintogerman281@gmail.com>
2024-08-02 12:20:08 -0300

    agregando funcionalidad de tiempo real

--- pages/mejoresvendedores.js
+++ pages/mejoresvendedores.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../components/Layout";
 import {
   BarChart,
@@ -25,7 +25,15 @@ const MEJORES_VENDEDORES = gql`
   }
 `;
 const MejoresVendedores = () => {
-  const { data, loading, error } = useQuery(MEJORES_VENDEDORES);
+  const { data, loading, error, startPolling, stopPolling } =
+    useQuery(MEJORES_VENDEDORES);
+
+  useEffect(() => {
+    startPolling(1000);
+    return () => {
+      stopPolling();
+    };
+  }, [startPolling, stopPolling]);
 
   if (loading) return "Cargando...";
   const { mejoresVendedores } = data;
